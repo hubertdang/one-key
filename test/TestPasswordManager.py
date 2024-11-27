da0884754ad5b96331a7773f04c9060a0a9ea17d
Hubert Dang <hubertdang@gmail.com>
2024-11-27 00:16:01 -0500

    I promise I will come back to finish this >_<

--- test/TestPasswordManager.py
+++ test/TestPasswordManager.py
@@ -33,22 +33,75 @@ class TestPasswordManager(unittest.TestCase):
         pm.add_user(u1)
 
         self.assertFalse(pm.remove_user(u2.get_username()))
-        self.assertTrue(pm.remove_user(u1.get_username()))
-        self.assertEqual(pm.get_user(u1.get_username()), None)
+        self.assertTrue(pm.is_valid_user(u1.get_username()))
+        self.assertEqual(pm.is_valid_user(u1.get_username()), None)
         self.assertFalse(pm.remove_user(u1.get_username()))
 
         pm.add_user(u1)
         pm.add_user(u2)
 
         self.assertTrue(pm.remove_user(u1.get_username()))
-        self.assertEqual(pm.get_user(u1.get_username()), None)
+        self.assertEqual(pm.is_valid_user(u1.get_username()), None)
         self.assertTrue(pm.remove_user(u2.get_username()))
-        self.assertEqual(pm.get_user(u2.get_username()), None)
+        self.assertEqual(pm.is_valid_user(u2.get_username()), None)
+
+    def test_is_valid_user(self):
+        pm = PasswordManager()
+        
+        u1 = User('hbrt', 'key')
+        u2 = User('saal', 'orange')
+
+        self.assertFalse(pm.is_valid_user('hbrt'))
+        
+        pm.add_user(u1)
+        self.assertTrue(pm.is_valid_user('hbrt'))
+
+        pm.add_user(u2)
+        self.assertTrue(pm.is_valid_user('saal'))
+        
+        pm.remove_user(u1)
+        self.assertFalse(pm.is_valid_user('hbrt'))
+
+        pm.remove_user(u2)
+        self.assertFalse(pm.is_valid_user('saa'))
 
     def test_sign_in(self):
         pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_sign_out(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_is_signed_in(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_get_key(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_set_key(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_get_username(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
 
     def test_set_username(self):
         pm = PasswordManager()
+        self.assertTrue(False)
 
+    def test_add_credential(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_remove_credential(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
+
+    def test_get_credential(self):
+        pm = PasswordManager()
+        self.assertTrue(False)
 
