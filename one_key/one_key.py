d8c29ffeb9011c48930ab95457527b164a54c69c
Hubert Dang <hubertdang@gmail.com>
2024-11-30 02:06:45 -0500

    initial commit

---
+++ one_key/one_key.py
@@ -0,0 +1,11 @@
+from one_key.User import User
+from one_key.Credential import Credential
+from one_key.PasswordManager import PasswordManager
+
+
+def main():
+    pm = PasswordManager()
+
+
+if __name__ == '__main__':
+    main()
