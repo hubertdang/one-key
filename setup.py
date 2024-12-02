2f985fb5a97daac70ebff8dd193546ef00176997
Hubert Dang <hubertdang@gmail.com>
2024-12-02 00:09:51 -0500

    update setup.py to add a little bit of extra info

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="one_key",
-    version="1.0.0",
+    name='one_key',
+    version='1.0.0',
+    author='Hubert Dang',
+    author_email='hubertdang@gmail.com',
+    description='A command line password manager',
+    long_description=open('README.md').read(),
     packages=find_packages(exclude=['test']),
     entry_points={
         'console_scripts': [
